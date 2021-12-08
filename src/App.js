2c1a251d8340fbedaac485486b9a9e9885ddf48f
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-12-08 14:04:04 +0000

    adding sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <h1>Hello</h1>
+        <picture style={{ width: '20em' }}>
+          <img src={logo} alt='React logo' />
+        </picture>
       </header>
+      <main>
+        <p>Hi there</p>
+      </main>
+      <footer>
+        <p>&copy; Robin James Kerrison 2021</p>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
