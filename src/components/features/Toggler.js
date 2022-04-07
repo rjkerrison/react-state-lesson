d18c6161e406c11d1bc46d1803cec4f6f4bcb688
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-07 13:22:17 +0200

    Toggler solution

--- src/components/features/Toggler.js
+++ src/components/features/Toggler.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import { useState } from 'react'
 import Button from '../common/Button'
 
 const Toggler = () => {
-  const value = false
+  const [value, setValue] = useState(false)
+
+  const toggleValue = () => {
+    // We can also pass React a function
+    // which calculates the new value
+    // based on the old one
+    setValue((currentValue) => !currentValue)
+  }
 
   return (
     <div>
       <h2>Toggler</h2>
       <div className='button-group'>
-        <Button text={value ? 'ON' : 'OFF'} />
+        <Button text={value ? 'ON' : 'OFF'} onClick={toggleValue} />
       </div>
     </div>
   )
